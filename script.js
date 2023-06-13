75470ba0b8dcc8e67a4226c3d15949ba70f195bd
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-13 20:08:02 +0100

    Start button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('HELLO WORLD')
+const words = [
+  'HELLO',
+  'WORLD',
+  'RHYTHM'
+]
+
+const startButton = document.getElementById('start-button')
+startButton.onclick = startGame
+
+function startGame() {
+  console.log('GAME ON')
+}
+
