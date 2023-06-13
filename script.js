a67ffa4c251961ff1bc40387fa3b59974fc724e8
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-13 20:09:53 +0100

    Pick a word to guess

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const words = [
   'RHYTHM'
 ]
 
+let wordToGuess
+
 const startButton = document.getElementById('start-button')
 startButton.onclick = startGame
 
 function startGame() {
   console.log('GAME ON')
+  const indexOfWord = Math.floor(Math.random() * words.length)
+  wordToGuess = words[indexOfWord]
+  console.log(indexOfWord)
 }
 
