fb0aac9bb878a11b67fdc373ff060a531023df72
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-13 20:19:15 +0100

    Register a guessed letter

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ const words = [
 ]
 
 let wordToGuess
+let lettersGuessedSoFar
 
 const startButton = document.getElementById('start-button')
+const guess = document.getElementById('guess')
+guess.hidden = true
 startButton.onclick = startGame
+guess.onkeydown = makeGuess
+
+function makeGuess(e) {
+  const guessedLetter = e.key
+  console.log(guessedLetter)
+  guess.value = ''
+}
 
 function startGame() {
   console.log('GAME ON')
   const indexOfWord = Math.floor(Math.random() * words.length)
   wordToGuess = words[indexOfWord]
-  console.log(indexOfWord)
+  lettersGuessedSoFar = []
+  guess.hidden = false
+  startButton.hidden = true
 }
 
